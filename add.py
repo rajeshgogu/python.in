afe66412e74d1bb398b73bfdff8ef35026ff7c1d
rajesh1234 <114799868+rajeshgogu@users.noreply.github.com>
2022-12-09 23:04:36 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,6 @@
+#Create two variables (x and y)and assign the values to them
+
+x = input("enter first number : ")
+y = input("enter second number : ")
+sum = int(firstnumber)+ (second number)
+print("the answer is : ")
---
+++ swapFunction.py
@@ -0,0 +1,14 @@
+#Take inputs from user for two numbers (x and y)
+x = 5
+y = 6
+print("before swap,value of x is :" ,x)
+print("before swap,value of y is :" ,y)
+temp = x
+print("value of temp is :" ,temp)
+x=y
+print("value of x is :" ,x)
+y = temp
+print("value of Y is :" ,y)
+
+print("AFTER SWAP,value of x is :" ,x)
+print("AFTER SWAP,value of Y is :" ,y)

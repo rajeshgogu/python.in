67022596288def292c6b6e65662173c0d0a4d244
rajesh1234 <114799868+rajeshgogu@users.noreply.github.com>
2022-12-09 22:41:31 +0530

    Add files via upload

---
+++ listmethods.py
@@ -0,0 +1,13 @@
+#lists are used to store multiple non homogeneous data in a single variable
+#ordered
+#changable
+#allows duplicate value
+# Insert()-insert item at specified index
+# Append()-insert item at the end of the list
+# Extend()-it appends elements from one list to another list,it can be used to append different collection
+# Remove()-remove item from a specified index
+# pop()-
+# clear()-clear all elements
+# range(),len()
+# sort()-it will sort the list in ascending order
+# 
